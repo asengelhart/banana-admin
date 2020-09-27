27cc2e1c4ad74fd10447e64ccd621e4240265182
asengelhart <asengelhart@yahoo.com>
2020-09-27 13:13:57 -0600

    basic layout

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {
+  Row,
+  Col,
+  Container
+} from 'reactstrap';
 import Icon from '../../Components/Icon';
+import Navbar from '../../Components/Navbar';
+import ApplicationCard from '../../Components/ApplicationCard';
+import API from '../../Services/API';
 
 export default function HomePage() {
+  const [clients, setClients] = useState(0);
+  const [donors, setDonors] = useState(0);
+  const [donations, setDonations] = useState({claimedtotal: 0, activetotal: 0});
+
+  useEffect(() => {
+    let id = JSON.parse(API.userToken()).admin.id;
+    API.jsonGet(`admins/${id}/get_new_clients`).then(json => {setClients(json.newclients)});
+    API.jsonGet(`admins/${id}/get_new_donors`).then(json => {setDonors(json.newdonors)});
+    API.jsonGet(`admins/${id}/get_donations_status`).then(json => {setDonations(json)});
+  });
+
   return (
     <div>
-      <Icon name="bananaIcon" size={36} />
-      <h1>
-        welcome to admin portal!
-      </h1>
+      <Navbar />
+      <Container>
+        <Row>
+          <Col lg={6}>
+
+          </Col>
+          <Col lg={6}>
+            
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
