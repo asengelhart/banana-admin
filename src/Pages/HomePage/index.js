a7764dcecad7bab6356a095a5f5ac78e079cf1bf
asengelhart <asengelhart@yahoo.com>
2020-10-07 18:32:12 -0600

    styling of page with application cards

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -8,32 +8,39 @@ import Icon from '../../Components/Icon';
 import Navbar from '../../Components/Navbar';
 import ApplicationCard from '../../Components/ApplicationCard';
 import API from '../../Services/API';
+import styles from './style.module.scss';
 
 export default function HomePage() {
-  const [clients, setClients] = useState(0);
-  const [donors, setDonors] = useState(0);
+  const [clients, setClients] = useState(5);
+  const [donors, setDonors] = useState(5);
   const [donations, setDonations] = useState({claimedtotal: 0, activetotal: 0});
 
-  useEffect(() => {
-    let id = JSON.parse(API.userToken()).admin.id;
-    API.jsonGet(`admins/${id}/get_new_clients`).then(json => {setClients(json.newclients)});
-    API.jsonGet(`admins/${id}/get_new_donors`).then(json => {setDonors(json.newdonors)});
-    API.jsonGet(`admins/${id}/get_donations_status`).then(json => {setDonations(json)});
-  });
+  // useEffect(() => {
+  //   let id = JSON.parse(API.userToken()).admin.id;
+  //   API.jsonGet(`admins/${id}/get_new_clients`).then(json => {setClients(json.newclients)});
+  //   API.jsonGet(`admins/${id}/get_new_donors`).then(json => {setDonors(json.newdonors)});
+  //   API.jsonGet(`admins/${id}/get_donations_status`).then(json => {setDonations(json)});
+  // });
 
   return (
     <div>
       <Navbar />
+      <div className={styles.container}>
       <Container>
         <Row>
           <Col lg={6}>
-
+            <h1 className={styles.title}>NEW APPLICATIONS</h1>
+            <ApplicationCard type='donor' userCount={donors} approvedCount={0}/>
+            <ApplicationCard type='client' userCount={clients} approvedCount={0}/>
           </Col>
           <Col lg={6}>
-            
+            <div>
+              <h2>Donation Card Here</h2>
+            </div>
           </Col>
         </Row>
       </Container>
+      </div>
     </div>
   );
 }
